fbf16bc47a53058cf3150dd2033796014f8f3fe1
Alvaro <dralvphd2@gmail.com>
2022-11-30 06:58:43 -0300

    Criando métodos para cadastrar e listar filmes

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,34 @@
+﻿
+using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController: ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
+
+        [HttpPost]
+        public void AdicionarFilme([FromBody] Filme filme)
+        {
+            filme.id = id++;
+            filmes.Add(filme);
+            
+        }
+
+        [HttpGet]
+        public List<Filme> RecuperaFilmes()
+        {
+            return filmes;
+        }
+        [HttpGet("{id}")]
+        public Filme RecuperarFilmesPorId(int id)
+        {
+            var filme  = filmes.Where(f=>f.id == id).FirstOrDefault();
+            return filme;
+        }
+    }
+}
